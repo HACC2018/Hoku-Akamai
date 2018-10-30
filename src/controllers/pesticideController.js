eab2561a7fe9a4394a6a4765e00b3634c6a6d184
Steve <lorenz21@hawaii.edu>
2018-10-30 00:31:40 -1000

    pesticide item

--- src/controllers/pesticideController.js
+++ src/controllers/pesticideController.js
@@ -13,10 +13,33 @@ module.exports = {
           }
       })
       .then(pesticides => {
-         pesticides.splice(0, 990);
-         console.log(pesticides[0].product_no)
-         res.render("pesticides/index", {pesticides});
+        //  let pesticidesList = pesticides.slice(10);
+         let pesticidesList = pesticides.filter(pesticide => pesticide.classific == 'Federal Restricted');
+         res.render("pesticides/index", {pesticidesList});
       })
-
+      .catch(error => {
+        console.log(`An error has occured: ${error}`)
+      });
+   },
+   show(req, res, next) {
+    let url = "https://data.hawaii.gov/resource/ufr5-uv4x.json?eparegisno="
+    fetch(`${url}${req.params.id}`)
+    .then( response => {
+       if(response.ok){
+          return response.json()
+        }
+        else{
+          return Promise.reject('Something went wrong!')
+        }
+    })
+    .then(pesticide => {
+       let pesticideItem = pesticide[0];
+       console.log(pesticideItem)
+       res.render("pesticides/show", {pesticideItem});
+    })
+    .catch(error => {
+      console.log(`An error has occured: ${error}`)
+    });
    }
+   
 }
